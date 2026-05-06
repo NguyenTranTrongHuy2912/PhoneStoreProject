e69ccb5e28dc9a4fe978945e7583c40d4b07c866
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2026-05-07 05:24:32 +0700

    feat: update docker-compose and Vite config for improved volume management and server settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Ép Vite phải quét file liên tục
+      interval: 100,    // Kiểm tra mỗi 100ms
+    },
+    host: true,         // Phải có để Docker mapping cổng
+    port: 5173,
+  }
 })
