a3db7bc320adc7b9baa748472ad45022929c9cfd
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2026-05-09 03:39:34 +0700

    feat(pages): init admin dashboard, product management and order management pages

---
+++ frontend/src/pages/AdminOrderPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminOrderPage() {
+  return (
+    <div>AdminOrderPage</div>
+  )
+}
+
+export default AdminOrderPage
---
+++ frontend/src/pages/AdminProductPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminProductPage() {
+  return (
+    <div>AdminProductPage</div>
+  )
+}
+
+export default AdminProductPage
